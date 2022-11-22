1f4e603f30023df532dd504c7a54bb0ca817d79b
AnteikuDevs (Teguh Sugiarto) <46734111+AnteikuDevs@users.noreply.github.com>
2022-11-22 15:46:27 +0700

    v1 release

--- file-upload.js
+++ file-upload.js
@@ -385,21 +385,6 @@ class FileUpload {
         }
     }
 
-    dragged(action = 'show')
-    {
-        let content = `<svg xml:space="preserve" viewBox="0 0 100 100" y="0px" x="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" id="圖層_1" version="1.1" width="200px" height="200px"
-        style="background-size:initial;background-repeat-y:initial;background-repeat-x:initial; -position-y:initial;background-position-x:initial;background-origin:initial;background-color:initial;background-clip:initial; -attachment:initial;animation-play-state:paused"><g class="ldl-scale" style="transform-origin:50% 50%;transform:rotate(0deg) scale(1, 1);animation-play-state:paused"><g style="animation-play-state:paused"><path d="M83,25H50v-3c0-3.866028-3.134033-7-7-7H17c-3.865967,0-7,3.133972-7,7v3v53c0,3.865967,3.134033,7,7,7h66 c3.865967,0,7-3.134033,7-7V32C90,28.133972,86.865967,25,83,25z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="3.5" stroke="#333435" fill="#F5E169" style="stroke:rgb(253, 253, 253);fill:rgb(187, 206, 221);animation-play-state:paused"></path> <path d="M83,85H17c-3.865993,0-7-3.13401-7-7V42c0-3.865993,3.134007-7,7-7h66c3.86599,0,7,3.134007,7,7v36C90,81.86599,86.86599,85,83,85z" stroke-miterlimit="10" stroke-linejoin="round" -linecap="round" stroke-width="3.5" stroke="#333435" fill="#F7B26A" style="stroke:rgb(253, 253, 253);fill:rgb(133, 162, 182); -play-state:paused"></path></g> <path d="M64.475098,62.475098 c-1.367188,1.366211-3.583008,1.366211-4.950195,0L53.5,56.450195V73c0,1.933105-1.566895,3.5-3.5,3.5s-3.5-1.566895-3.5-3.5 V56.450195l-6.024902,6.024902c-1.367188,1.366211-3.583008,1.366211-4.950195,0c-1.366699-1.367188-1.366699-3.583008,0-4.950195 l12-12c0.011902-0.011902,0.026245-0.019531,0.038269-0.03125c0.152893-0.148682,0.317322-0.285278,0.495239-0.404236 c0.086853-0.058044,0.18103-0.097961,0.271667-0.147461c0.109619-0.060059,0.215027-0.126099,0.33136-0.174316 c0.114624-0.047424,0.234314-0.074707,0.352417-0.109375c0.100525-0.029724,0.196838-0.068481,0.300903-0.089172 C49.540894,44.524109,49.770325,44.5,50,44.5s0.459106,0.024109,0.685242,0.069092 c0.104065,0.020691,0.200378,0.059448,0.300903,0.089172c0.118103,0.034668,0.237793,0.061951,0.352417,0.109375 c0.116333,0.048218,0.221741,0.114258,0.33136,0.174316c0.090637,0.0495,0.184814,0.089417,0.271667,0.147461 c0.177917,0.118958,0.342346,0.255554,0.495239,0.404236c0.012024,0.011719,0.026367,0.019348,0.038269,0.03125l12,12 C65.841797,58.89209,65.841797,61.10791,64.475098,62.475098z" stroke-miterlimit="10" stroke-linecap="round" stroke-width="3.5" stroke="#333435" fill="#ACBD81" style="stroke:rgb(253, 253, 253);fill:rgb(220, 228, 235);animation-play-state:paused"></path></g></svg>`
-
-        if(action == 'show')
-        { 
-            $(this.contentDragId).css('zIndex',1)
-            $(this.contentDragId).html(content)
-        }else{
-            $(this.contentDragId).css('zIndex','-1')
-            $(this.contentDragId).html('')
-        }
-    }
-
     drawContent()
     {
 
@@ -413,10 +398,7 @@ class FileUpload {
         let contentIcon = `<div class="file__upload_icon"><div class="file__upload_icon_svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M152 120c-26.51 0-48 21.49-48 48s21.49 48 48 48s48-21.49 48-48S178.5 120 152 120zM447.1 32h-384C28.65 32-.0091 60.65-.0091 96v320c0 35.35 28.65 64 63.1 64h384c35.35 0 64-28.65 64-64V96C511.1 60.65 483.3 32 447.1 32zM463.1 409.3l-136.8-185.9C323.8 218.8 318.1 216 312 216c-6.113 0-11.82 2.768-15.21 7.379l-106.6 144.1l-37.09-46.1c-3.441-4.279-8.934-6.809-14.77-6.809c-5.842 0-11.33 2.529-14.78 6.809l-75.52 93.81c0-.0293 0 .0293 0 0L47.99 96c0-8.822 7.178-16 16-16h384c8.822 0 16 7.178 16 16V409.3z"/></svg></div>${highlightMsg}</div>`
         let _contentId = '__'+file_upload_GenerateRandId(4)+'__'
         this.contentId = '['+_contentId+']'
-        let content = `<div class="file__upload_content" ${_contentId}>${contentIcon}${this.drawContent_info()}${this.drawContent_action()}${this.drawContent_loading()}${this.drawContent_dragEvent()}</div>
-        ${this.drawError()}
-        ${this.drawResult()}
-        `
+        let content = `<div class="file__upload_content" ${_contentId}>${contentIcon}${this.drawContent_info()}${this.drawContent_action()}${this.drawContent_loading()}${this.drawContent_dragEvent()}</div>${this.drawError()}${this.drawResult()}`
 
         this.renderEventDragAndDrop()
 
@@ -453,7 +435,7 @@ class FileUpload {
 
         file_upload_createEvent(this.contentId,'dragover', function(e,_elThis){
             e.preventDefault();
-            _this.dragged('show')
+
             $(_elThis).addClass('drag')
             $(_this.browseId).addClass('disabled')
         })
@@ -461,7 +443,6 @@ class FileUpload {
             e.preventDefault();
             
             $(_elThis).removeClass('drag')
-            _this.dragged('hide')
             $(_this.browseId).removeClass('disabled')
         })
         file_upload_createEvent(this.contentId,'drop', function(e,_elThis){
@@ -472,7 +453,6 @@ class FileUpload {
             $(_elThis).addClass('drop')
             _this.renderUpload(files)
             $(_this.browseId).removeClass('disabled')
-            _this.dragged('hide')
         })
 
     }
@@ -696,9 +676,7 @@ class FileUpload {
 
     previewFile(data)
     {
-        let previewContent = `
-            <img src="${data.data}" alt="${data.anme}">
-        `
+        let previewContent = `<img src="${data.data}" alt="${data.name}">`
 
         if(data.fileType == 'video')
         {
